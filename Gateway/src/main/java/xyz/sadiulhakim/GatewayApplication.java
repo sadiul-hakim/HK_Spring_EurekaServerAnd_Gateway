c7eaa2cd6d758160bd2bc2f694e259590d49d340
Sadiul Hakim <sadiulhakim@gmail.com>
2025-04-04 23:06:31 +0600

    Load Balancer

--- Gateway/src/main/java/xyz/sadiulhakim/GatewayApplication.java
+++ Gateway/src/main/java/xyz/sadiulhakim/GatewayApplication.java
@@ -2,14 +2,11 @@ package xyz.sadiulhakim;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
-
 }
